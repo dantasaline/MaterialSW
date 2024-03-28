c8c2f362ae32c66679df6e3a19f47bb853a91497
Aline Dantas <124791771+dantasaline@users.noreply.github.com>
2024-03-28 08:31:34 -0300

    Add files via upload

---
+++ banco/Conta.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace banco
+{
+    class Conta
+    {
+        public string? num_conta { get; set; }
+
+        private float saldo = 1200;
+
+        private float limite = 500;
+
+
+        public void olha_saldo()
+        {
+
+            Console.WriteLine("seu saldo é de: R$" + saldo);
+
+        }
+
+        public void deposito(float deposit)
+        {
+
+            saldo += deposit;
+            Console.WriteLine("Seu saldo agora é de: R$" + saldo);
+            Console.WriteLine("Você pode sacar até R$" + (saldo + limite));
+
+        }
+
+        public void saque(float saque)
+        {
+
+            if (saque <= saldo + limite)
+            {
+
+                saldo -= saque;
+                Console.WriteLine("Seu saldo após o saque é de: R$" + saldo);
+
+            }
+
+            else 
+            {
+
+                Console.WriteLine("Saque não disponivel, valor solicitado está além do limite");
+
+            }
+
+        }
+
+    } 
+
+}
+        
+
+        
+
---
+++ banco/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta();
+
+            conta.deposito(200);
+
+            conta.olha_saldo();
+
+            conta.saque(2500);
+
+            conta.olha_saldo();
+        }
+    }
+}
