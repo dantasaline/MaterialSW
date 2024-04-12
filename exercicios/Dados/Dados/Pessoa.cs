7d171053b41b6e949c308ab4d95bb8e4fab704d3
Aline Dantas <124791771+dantasaline@users.noreply.github.com>
2024-04-12 11:40:57 -0300

    Add files via upload

---
+++ exercicios/Dados/Dados/Pessoa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Dados
+{
+    internal class Pessoa
+    {
+        private string? Nome { get; set; }
+        private DateTime Nascimento { get; set; }
+        private Double Altura { get; set; }
+        private int Idade {  get; set; }
+        public string Info()
+        {
+            Nome = "Aline";
+            Nascimento =  new DateTime(2007, 7, 26);
+            Altura = Convert.ToDouble(1.73);
+
+            return $"Nome: {Nome}\nNascimento: {Nascimento}\nAltura: {Altura}";
+        }
+
+        public string CalcIdade()
+        {
+            int Ano = 2024;
+            Idade = Ano - Nascimento.Year;
+
+            return $"Idade: {Idade}";
+        }
+
+    }
+}
---
+++ exercicios/Dados/Dados/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace Dados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dados da pessoa");
+            Pessoa a = new Pessoa();
+            Console.WriteLine(a.Info());
+            Console.WriteLine(a.CalcIdade());
+        }
+    }
+}
