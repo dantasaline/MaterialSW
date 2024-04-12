c3a52d788eab58262035df64541557dc4c568d96
Aline Dantas <124791771+dantasaline@users.noreply.github.com>
2024-04-12 11:52:20 -0300

    Add files via upload

---
+++ exercicios/Album_de_fotos/AlbumdFotos/Album.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlbumdFotos
+{
+    internal class Album
+    {
+        public double Nfotos { get; set; }
+        public double Pag { get; set; }
+        public double resto { get; set; }
+
+        public string Pagina()
+        {
+
+            Console.WriteLine("Quantas fotos você vai usar?");
+            Nfotos = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Quantas fotos por página?");
+            Pag = Convert.ToDouble(Console.ReadLine());
+
+            resto = Nfotos % Pag;
+
+            if (resto == 1)
+            {
+                return "Você pode colocar " + Nfotos / Pag + " e vai sobrar " + resto + " fotos";
+            }
+            else
+            {
+                return "Você pode colocar " + Nfotos / Pag + " fotos por página";
+            }
+        }
+    }
+}
---
+++ exercicios/Album_de_fotos/AlbumdFotos/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AlbumdFotos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculando numero de paginas:");
+            Album album = new Album();
+
+            Console.WriteLine(album.Pagina());
+        }
+    }
+}
