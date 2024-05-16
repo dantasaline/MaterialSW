478b7c26a1ec315d6df207080f38c135617d9525
Aline Dantas <124791771+dantasaline@users.noreply.github.com>
2024-05-16 10:33:19 -0300

    Add files via upload

---
+++ Construtores/ex1/Estoque/Estoque/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Estoque
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto1 = new Produto();
+
+            // Exibindo informações do produto
+            produto1.Exibir();
+
+            // Adicionando itens ao estoque
+            produto1.Adicionar(500);
+            produto1.Exibir();
+
+            // Removendo itens do estoque
+            produto1.Remover(430);
+            produto1.Exibir();
+
+            Console.ReadLine();
+        }
+    }
+    
+}
