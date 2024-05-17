84d11d487fb657756fa8139a3f68eff89b8053b7
Aline Dantas <124791771+dantasaline@users.noreply.github.com>
2024-05-17 19:21:46 -0300

    Add files via upload

---
+++ Construtores/ex10/zoologico/Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zoologico
+{
+    class Animal
+    {
+        public string Nome { get; set; }
+        public string Especie { get; set; }
+
+        public string Som {  get; set; }
+        public int Idade { get; set; }
+
+        
+
+        public Animal(string nome, string especie, int idade, string som)
+        {
+            Nome = nome;
+            Especie = especie;
+            Idade = idade;
+            Som = som;
+        }
+
+        public void EmitirSom()
+        {
+            
+            Console.WriteLine($"{Nome} faz {Som}.");
+        }
+    }
+}
---
+++ Construtores/ex10/zoologico/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace zoologico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cachorro = new Animal("Sirius", "Cachorro", 5, "AUAU");
+            cachorro.EmitirSom();
+
+            Animal gato = new Animal("Jasper", "Gato", 3,"MIAU" );
+            gato.EmitirSom();
+
+            Animal papagaio = new Animal("Loro", "Papagaio", 7, "Loro quer biscoito");
+            papagaio.EmitirSom();
+        }
+    }
+}
