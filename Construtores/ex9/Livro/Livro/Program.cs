5521503c7201aa08098c4a8c1cb65ffbefea59d2
Aline Dantas <124791771+dantasaline@users.noreply.github.com>
2024-05-17 19:08:28 -0300

    Add files via upload

---
+++ Construtores/ex9/Livro/Livro/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Livro> livros = new List<Livro>();
+
+            while (true)
+            {
+                Console.WriteLine("\nSelecione uma opção:");
+                Console.WriteLine("1. Adicionar Livro");
+                Console.WriteLine("2. Sair");
+
+                string opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1":
+                        Console.WriteLine("Digite o título do livro:");
+                        string tituloLivro = Console.ReadLine();
+                        Console.WriteLine("Digite o autor do livro:");
+                        string autorLivro = Console.ReadLine();
+                        Livro novoLivro = new Livro(tituloLivro, autorLivro);
+                        livros.Add(novoLivro);
+                        Console.WriteLine("Livro adicionado com sucesso.");
+                        break;
+                    case "2":
+                        Console.WriteLine("Saindo...");
+                        return;
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Construtores/ex9/Livro/Livro/o.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex9
+{
+    public class Livro
+    {
+        private string titulo;
+        private string autor;
+
+        public string Titulo
+        {
+            get { return titulo; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("O título do livro não pode ser vazio.");
+                }
+                titulo = value;
+            }
+        }
+
+        public string Autor
+        {
+            get { return autor; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("O autor do livro não pode ser vazio.");
+                }
+                autor = value;
+            }
+        }
+
+        public Livro(string titulo, string autor)
+        {
+            Titulo = titulo;
+            Autor = autor;
+        }
+    }
+}
