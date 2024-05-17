5d6ddf0762acbf9760120ecb667f43ca9b09901e
Aline Dantas <124791771+dantasaline@users.noreply.github.com>
2024-05-17 19:00:55 -0300

    Add files via upload

---
+++ Construtores/exerc11/paineldecarro/Carro.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paineldecarro
+{
+    class Carro
+    {
+        public string Modelo { get; set; }
+        public int Ano { get; set; }
+        private int velocidade;
+
+        public int Velocidade
+        {
+            get { return velocidade; }
+            private set { velocidade = value; }
+        }
+
+        public Carro(string modelo, int ano)
+        {
+            Modelo = modelo;
+            Ano = ano;
+            Velocidade = 0;
+        }
+
+        public void Acelerar()
+        {
+            Velocidade += 10;
+        }
+
+        public void Frear()
+        {
+            Velocidade -= 10;
+            if (Velocidade < 0)
+            {
+                Velocidade = 0;
+            }
+        }
+    }
+}
---
+++ Construtores/exerc11/paineldecarro/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace paineldecarro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro("Fusca", 1975);
+
+            Console.WriteLine($"Velocidade inicial do {carro.Modelo}: {carro.Velocidade} km/h");
+
+            carro.Acelerar();
+            Console.WriteLine($"Velocidade após acelerar: {carro.Velocidade} km/h");
+
+            carro.Frear();
+            Console.WriteLine($"Velocidade após frear: {carro.Velocidade} km/h");
+
+            // Tentando frear novamente para garantir que a velocidade não se torne negativa
+            carro.Frear();
+            Console.WriteLine($"Velocidade após tentar frear novamente: {carro.Velocidade} km/h");
+        }
+    }
+}
