25a17aeea9775aaeb2415a31e06419a094d33236
dantasaline <alinedantasdl@gmail.com>
2024-09-19 12:39:43 -0300

    atualizei o site

--- site/painel/processa_login.php
+++ site/painel/processa_login.php
@@ -1,27 +1,24 @@
 <?php
+
+    session_start();
+
     require('conecta.php');
     $email = $_POST['email'];
-    $senha = $_POST['senha'];
+    $senha = md5($_POST['senha']);
 
     $consulta = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
 
     $resultado = $conexao->query($consulta);
-
-    //echo "Numeros de registros ==> ". $resultado;
-    //var_dump($resultado);
-
-
     $registros = $resultado->num_rows;
-
-    //echo "Numeros de registros ==> ". $registros;
-
     $resultado_usuario = mysqli_fetch_assoc($resultado);
     
     //var_dump($resultado_usuario);
 
     if($registros == 1){
-        //echo "TE ACHEI ==> " . $resultado_usuario['nome'];       
-               
+        $_SESSION['id'] = $resultado_usuario['id'];
+        $_SESSION['nome'] = $resultado_usuario['nome'];
+        $_SESSION['email'] = $resultado_usuario['email'];
+
         header('Location: index.php');
 
     }
